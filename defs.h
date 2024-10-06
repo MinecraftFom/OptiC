94582a8a99219c1f9a7b1b70b4f63320e3ad48e5
MinecraftFom <minecraft_fom@163.com>
2024-10-06 20:14:24 +0800

    Add files via upload

---
+++ defs.h
@@ -0,0 +1,149 @@
+#ifndef C23LIBSREBUILD_DEFS_H
+#define C23LIBSREBUILD_DEFS_H
+
+#define cls      class
+#define crt      struct
+#define dl       while bss 1 bse
+#define let      auto
+#define gt       typeof
+#define td       typedef
+#define use      struct
+#define cls      class
+#define crt      struct
+#define dl       while bss 1 bse
+#define let      auto
+#define gt       typeof
+#define td       typedef
+#define use      struct
+
+#define false    bss NULL isnt NULL bse
+#define equ      ==
+#define isnt     !=
+#define NULL     0x00
+#define syns     :
+#define ed       ;
+#define true     bss NULL is NULL bse
+#define val      =
+
+#define pint     it p
+#define strg     co ch p
+
+#define bs       [
+#define be       ]
+#define bss      (
+#define bse      )
+#define s        {
+#define e        }
+#define c        ,
+#define cp       .
+#define pcp      ->
+#define and      &&
+#define or       ||
+#define sw       switch
+#define ca       case
+#define df       default   syns
+#define p        *
+#define gl       &
+#define pub      public    syns
+#define prv      private   syns
+#define prot     protected syns
+
+#define by       *
+#define dv       /
+#define mn       -
+#define pl       +
+#define of       :
+#define in       ::
+
+#define addr     ++
+#define minr     --
+
+#define ae       +=
+#define me       -=
+#define bye      *=
+#define de       /=
+
+#define ext      extern
+#define em       enum
+#define un       union
+#define co       const
+#define ch       char
+#define it       int
+#define db       double
+#define fl       float
+#define inl      inline
+#define lg       long
+#define res      restrict
+#define sh       short
+#define sd       signed
+#define sta      static
+#define v        void
+#define vt       volatile
+#define vret     return ed
+#define ret      return
+#define false    bss NULL isnt NULL bse
+#define is       ==
+#define isnt     !=
+#define NULL     0x00
+#define syns     :
+#define ed       ;
+#define true     bss NULL is NULL bse
+
+#define pint     it p
+#define strg     co ch p
+
+#define bs       [
+#define be       ]
+#define bss      (
+#define bse      )
+#define s        {
+#define e        }
+#define c        ,
+#define cp       .
+#define pcp      ->
+#define and      &&
+#define or       ||
+#define sw       switch
+#define ca       case
+#define df       default   syns
+#define p        *
+#define gl       &
+#define pub      public    syns
+#define prv      private   syns
+#define prot     protected syns
+
+#define by       *
+#define dv       /
+#define mn       -
+#define pl       +
+#define of       :
+#define in       ::
+
+#define addr     ++
+#define minr     --
+
+#define ae       +=
+#define me       -=
+#define bye      *=
+#define de       /=
+
+#define ext      extern
+#define em       enum
+#define un       union
+#define co       const
+#define ch       char
+#define it       int
+#define db       double
+#define fl       float
+#define inl      inline
+#define lg       long
+#define res      restrict
+#define sh       short
+#define sd       signed
+#define sta      static
+#define v        void
+#define vt       volatile
+#define vret     return ed
+#define ret      return
+
+#endif //C23LIBSREBUILD_DEFS_H
